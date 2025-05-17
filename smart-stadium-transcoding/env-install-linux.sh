f409b2cf726b3d73361b479beb297bd949e17744
Xiao Zhang <zx123@utexas.edu>
2025-05-17 17:43:38 -0500

    chore: Add environment setup for linux

---
+++ smart-stadium-transcoding/env-install-linux.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y update
+sudo apt install -y make g++ ffmpeg pkg-config
+sudo apt install -y libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev libswscale-dev libavdevice-dev
+
