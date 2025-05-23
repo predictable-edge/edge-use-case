979f40e089b1bc83234fbbcfc8eed90fb880853f
Xiao Zhang <zx123@utexas.edu>
2025-05-22 22:44:49 -0500

    chore: Reduce the bitrate to reduce computing overhead

--- transcoding-apps/transcoding/transcoding-rtp/transcoding-rtp.cpp
+++ transcoding-apps/transcoding/transcoding-rtp/transcoding-rtp.cpp
@@ -1037,7 +1037,7 @@ int main(int argc, char* argv[]) {
     };
 
     std::vector<ResolutionBitrateLog> resolution_bitrate_log = {
-        {3840, 2160, 16000,  "frame-1"},
+        {1920, 1080, 8000,  "frame-1"},
         {2560, 1440, 10000,  "frame-2"},
         {1920, 1080, 5000,  "frame-3"},
         {1280, 720,  2500,  "frame-4"},
