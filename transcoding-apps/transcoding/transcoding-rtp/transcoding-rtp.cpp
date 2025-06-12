7ed51bb477b44511e8712b2175c121a7d3f1ac51
Xiao Zhang <zx123@utexas.edu>
2025-06-12 16:48:45 -0500

    chore: Update the transcoding code with measurement ones

--- transcoding-apps/transcoding/transcoding-rtp/transcoding-rtp.cpp
+++ transcoding-apps/transcoding/transcoding-rtp/transcoding-rtp.cpp
@@ -287,7 +287,7 @@ bool initialize_decoder(const char* input_url, DecoderInfo& decoder_info) {
     // av_dict_set(&format_opts, "rtsp_transport", "udp", 0);       // Use UDP for RTP transport
     // av_dict_set(&format_opts, "rtsp_flags", "prefer_tcp", 0);    // Prefer TCP for RTSP control connection
     av_dict_set(&format_opts, "buffer_size", "8192000", 0);      // Increase buffer size
-    av_dict_set(&format_opts, "max_delay", "500000", 0);         // 500ms max delay
+    av_dict_set(&format_opts, "max_delay", "200000", 0);         // 200ms max delay
     av_dict_set(&format_opts, "reorder_queue_size", "100000", 0);    // Reorder queue size
     av_dict_set(&format_opts, "jitter_buffer_size", "100000", 0);    // Jitter buffer size
     av_dict_set(&format_opts, "stimeout", "5000000", 0);         // Socket timeout 5 seconds
